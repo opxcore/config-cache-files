13b1bda8dc92b3dee8baac37f69619da09eda0e4
Slava <lozovoyv@gmail.com>
2019-01-21 06:16:17 +0300

    Add logics

--- src/ConfigCacheFile.php
+++ src/ConfigCacheFile.php
@@ -46,12 +46,24 @@ class ConfigCacheFile implements ConfigCacheRepositoryInterface
      *
      * @param  string $id
      * @param  array $config
-
+     *
      * @return  bool
      */
     public function load(&$config, $id = 'default'): bool
     {
+        if (!$this->path) {
+            return false;
+        }
 
+        $filename = $this->makeFilename($id);
+
+        if (!file_exists($filename)) {
+            return false;
+        }
+
+        $config = unserialize(file_get_contents($filename), ['allowed_classes' => false]);
+
+        return true;
     }
 
     /**
@@ -64,6 +76,27 @@ class ConfigCacheFile implements ConfigCacheRepositoryInterface
      */
     public function save($config, $id = 'default'): bool
     {
+        if (!$this->path || (!is_dir($this->path) && !mkdir($this->path, 0644, true))) {
+            return false;
+        }
 
+        $filename = $this->makeFilename($id);
+
+        return file_put_contents($filename, serialize($config)) !== false;
+    }
+
+    /**
+     * Make filename for given id.
+     *
+     * @param  string $id
+     *
+     * @return  string
+     */
+    protected function makeFilename($id): string
+    {
+        $prefix = $this->prefix ? $this->prefix . '.' : null;
+        $extension = $this->extension ? $this->extension . '.' : null;
+
+        return $this->path . DIRECTORY_SEPARATOR . $prefix . $id . $extension;
     }
 }
