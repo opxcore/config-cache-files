0606e53c8f775d4e4317b207cefb8fd40c4c503b
lozovoyv <lozovoyv@gmail.com>
2021-02-02 14:21:18 +0300

    Update tests

--- tests/ConfigCacheFileTest.php
+++ tests/ConfigCacheFileTest.php
@@ -1,13 +1,14 @@
 <?php
 
+use OpxCore\Config\ConfigCacheFile;
 use PHPUnit\Framework\TestCase;
 
 class ConfigCacheFileTest extends TestCase
 {
-    protected $path;
-    protected $temp;
+    protected string $path;
+    protected string $temp;
 
-    public function setUp()
+    protected function setUp(): void
     {
         $this->path = __DIR__ . DIRECTORY_SEPARATOR . 'cache';
         $this->temp = sys_get_temp_dir();
@@ -15,116 +16,109 @@ class ConfigCacheFileTest extends TestCase
 
     public function test_Wrong_Path(): void
     {
-        $conf = new \OpxCore\Config\ConfigCacheFile($this->path . 'wrong');
+        $conf = new ConfigCacheFile($this->path . 'wrong');
 
         $config = [];
         $loaded = $conf->load($config);
-        $this->assertFalse($loaded);
-        $this->assertEquals([], $config);
+        self::assertFalse($loaded);
+        self::assertEquals([], $config);
     }
 
     public function test_Null_Path(): void
     {
-        $conf = new \OpxCore\Config\ConfigCacheFile(null);
+        $conf = new ConfigCacheFile(null);
         $config = [];
         $loaded = $conf->load($config);
-        $this->assertFalse($loaded);
-        $this->assertEquals([], $config);
+        self::assertFalse($loaded);
+        self::assertEquals([], $config);
     }
 
     public function test_File_Not_Exists(): void
     {
-        $conf = new \OpxCore\Config\ConfigCacheFile(__DIR__);
+        $conf = new ConfigCacheFile(__DIR__);
         $config = [];
         $loaded = $conf->load($config, 'empty');
-        $this->assertFalse($loaded);
-        $this->assertEquals([], $config);
+        self::assertFalse($loaded);
+        self::assertEquals([], $config);
     }
 
     public function test_Save(): void
     {
-        $path = $this->temp . DIRECTORY_SEPARATOR . 'test';
-        $conf = new \OpxCore\Config\ConfigCacheFile($path);
+        $path = $this->temp;
+        $conf = new ConfigCacheFile($path);
         $config = ['test' => 'ok'];
         $saved = $conf->save($config);
-        $this->assertTrue($saved);
-        $this->assertFileExists($path . DIRECTORY_SEPARATOR . 'config.cache');
-        $this->assertFileIsReadable($path . DIRECTORY_SEPARATOR . 'config.cache');
-        $this->assertStringEqualsFile($path . DIRECTORY_SEPARATOR . 'config.cache', 'a:2:{s:7:"expires";N;s:6:"config";a:1:{s:4:"test";s:2:"ok";}}');
+        self::assertTrue($saved);
+        self::assertFileExists($path . DIRECTORY_SEPARATOR . 'config.cache');
+        self::assertFileIsReadable($path . DIRECTORY_SEPARATOR . 'config.cache');
+        self::assertStringEqualsFile($path . DIRECTORY_SEPARATOR . 'config.cache', 'a:2:{s:7:"expires";N;s:6:"config";a:1:{s:4:"test";s:2:"ok";}}');
         unlink($path . DIRECTORY_SEPARATOR . 'config.cache');
-        rmdir($path);
     }
 
     public function test_Save_Profile(): void
     {
-        $path = $this->temp . DIRECTORY_SEPARATOR . 'test';
-        $conf = new \OpxCore\Config\ConfigCacheFile($path);
+        $path = $this->temp . DIRECTORY_SEPARATOR . 'cache';
+        $conf = new ConfigCacheFile($path);
         $config = ['test' => 'ok'];
         $saved = $conf->save($config, 'profile');
-        $this->assertTrue($saved);
-        $this->assertFileExists($path . DIRECTORY_SEPARATOR . 'config.profile.cache');
-        $this->assertFileIsReadable($path . DIRECTORY_SEPARATOR . 'config.profile.cache');
-        $this->assertStringEqualsFile($path . DIRECTORY_SEPARATOR . 'config.profile.cache', 'a:2:{s:7:"expires";N;s:6:"config";a:1:{s:4:"test";s:2:"ok";}}');
+        self::assertTrue($saved);
+        self::assertFileExists($path . DIRECTORY_SEPARATOR . 'config.profile.cache');
+        self::assertFileIsReadable($path . DIRECTORY_SEPARATOR . 'config.profile.cache');
+        self::assertStringEqualsFile($path . DIRECTORY_SEPARATOR . 'config.profile.cache', 'a:2:{s:7:"expires";N;s:6:"config";a:1:{s:4:"test";s:2:"ok";}}');
         unlink($path . DIRECTORY_SEPARATOR . 'config.profile.cache');
         rmdir($path);
     }
 
     public function test_Save_No_Folder(): void
     {
-        $path = $this->temp . DIRECTORY_SEPARATOR . 'test' . DIRECTORY_SEPARATOR . 'test';
-        $conf = new \OpxCore\Config\ConfigCacheFile($path);
+        $path = $this->temp;
+        $conf = new ConfigCacheFile($path);
         $config = ['test' => 'ok'];
         $saved = $conf->save($config);
-        $this->assertTrue($saved);
+        self::assertTrue($saved);
         unlink($path . DIRECTORY_SEPARATOR . 'config.cache');
-        rmdir($path);
-        rmdir($this->temp . DIRECTORY_SEPARATOR . 'test');
     }
 
     public function test_Load(): void
     {
-        $conf = new \OpxCore\Config\ConfigCacheFile($this->path);
+        $conf = new ConfigCacheFile($this->path);
         $config = [];
         $loaded = $conf->load($config);
-        $this->assertTrue($loaded);
-        $this->assertEquals(['test' => 'ok'], $config);
+        self::assertTrue($loaded);
+        self::assertEquals(['test' => 'ok'], $config);
     }
 
     public function test_Load_Profile(): void
     {
-        $conf = new \OpxCore\Config\ConfigCacheFile($this->path);
+        $conf = new ConfigCacheFile($this->path);
         $config = [];
         $loaded = $conf->load($config, 'profile');
-        $this->assertTrue($loaded);
-        $this->assertEquals(['test' => 'ok'], $config);
+        self::assertTrue($loaded);
+        self::assertEquals(['test' => 'ok'], $config);
     }
 
     public function test_Ttl_Ok(): void
     {
-        $path = $this->temp . DIRECTORY_SEPARATOR . 'test' . DIRECTORY_SEPARATOR . 'test';
-        $conf = new \OpxCore\Config\ConfigCacheFile($path);
+        $path = $this->temp;
+        $conf = new ConfigCacheFile($path);
         $config = ['test' => 'ok'];
         $saved = $conf->save($config, null, 100);
-        $this->assertTrue($saved);
+        self::assertTrue($saved);
         $loaded = $conf->load($config);
-        $this->assertTrue($loaded);
+        self::assertTrue($loaded);
         unlink($path . DIRECTORY_SEPARATOR . 'config.cache');
-        rmdir($path);
-        rmdir($this->temp . DIRECTORY_SEPARATOR . 'test');
     }
 
     public function test_Ttl_Not_Ok(): void
     {
-        $path = $this->temp . DIRECTORY_SEPARATOR . 'test' . DIRECTORY_SEPARATOR . 'test';
-        $conf = new \OpxCore\Config\ConfigCacheFile($path);
+        $path = $this->temp;
+        $conf = new ConfigCacheFile($path);
         $config = ['test' => 'ok'];
         $saved = $conf->save($config, null, 1);
-        $this->assertTrue($saved);
+        self::assertTrue($saved);
         sleep(2);
         $loaded = $conf->load($config);
-        $this->assertFalse($loaded);
+        self::assertFalse($loaded);
         unlink($path . DIRECTORY_SEPARATOR . 'config.cache');
-        rmdir($path);
-        rmdir($this->temp . DIRECTORY_SEPARATOR . 'test');
     }
 }
